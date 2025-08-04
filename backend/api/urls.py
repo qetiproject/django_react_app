60f6321dfbe384df28f0892f5a86939778ab33ff
Qeti Khetsuriani <ketideveloper@gmail.com>
2025-08-04 17:14:37 +0400

    migrations

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlPatterns = [
+urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>", views.NoteDelete.as_view(), name="delete-note")
 ]
